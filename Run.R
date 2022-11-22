d0bd16d15a5cc4e5197b31507c8da30121b0ba77
Daniel Maciel <damaciel_maciel@hotmail.com>
2022-11-22 08:27:40 -0500

    Small changes
    Updates to use csv, added final csv file

--- Run.R
+++ Run.R
@@ -21,7 +21,7 @@ source('R/UV_Slope.R')
 
 #Read the Gloria Rrs file in Excel format
 # SORRY, DANIEL: PLEASE ADD  CSV FILE READING (leave teh Excel file reading in comment, maybe). 
-gloria_rrs = openxlsx::read.xlsx('GLORIA_global_remote_sensing_reflectance_radiometry.xlsx', sheet = 'Rrs')
+gloria_rrs = fread('GLORIA_global_remote_sensing_reflectance_radiometry.csv')
 
 
 #Basline Calculation
@@ -59,16 +59,39 @@ filter(NOISE_UV_EDGE, flag == 1) %>% nrow()
 filter(SLOPE_UV, flag == 1) %>% nrow() 
 
 
-baseline_shifts
-oxygen_peak$Oxy_peak_height
-NOISE_RED_EDGE$flag
-NOISE_UV_EDGE$flag
-SLOPE_UV$flag
-
-#Save results
+#Save separated results
 
 write.csv(file = 'Outputs/baseline_shift.csv', x = baseline_shifts, na = 'NaN')
 write.csv(file = 'Outputs/oxygen_peak.csv', x = oxygen_peak, na = 'NaN')
 write.csv(file = 'Outputs/noise_red_edge.csv', x = NOISE_RED_EDGE, na = 'NaN')
 write.csv(file = 'Outputs/noise_uv_edge.csv', x = NOISE_UV_EDGE, na = 'NaN')
 write.csv(file = 'Outputs/slope_UV.csv', x = SLOPE_UV, na = 'NaN')
+
+
+## Merged Results 
+
+final_results = data.frame(GLORIA_ID = baseline_shifts$GLORIA_ID, 
+                           Baseline_shift = baseline_shifts$Baseline_shift, 
+                           Oxygen_Peak = oxygen_peak$flag, 
+                           Noisy_RedEdge = NOISE_RED_EDGE$flag,
+                           Noisy_Uv = NOISE_UV_EDGE$flag, 
+                           Slope_UV = SLOPE_UV$flag)
+
+# Check Final Results 
+
+filter(final_results, Baseline_shift == 1) %>% nrow() 
+filter(final_results, Oxygen_Peak == 1) %>% nrow() 
+filter(final_results, Noisy_RedEdge == 1) %>% nrow() 
+filter(final_results, Noisy_Uv == 1) %>% nrow() 
+filter(final_results, Slope_UV == 1) %>% nrow() 
+
+# Save all results 
+
+write.csv(file = 'Outputs/GLORIA_qc_flags.csv', x = final_results, na = 'NaN')
+
+
+
+
+
+
+
